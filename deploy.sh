4efa46b863d8cb42e2b4539457cf787ee7d310c6
Danilo Sato <dtsato@thoughtworks.com>
2018-06-05 15:58:28 -0700

    Scripts to deploy canary release and complete it

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ kubectl apply -f kubernetes/mysql.yml --namespace default
 
 IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
 PROJECT_ID=${GCLOUD_PROJECT_ID:-devops-workshop-123}
-echo "Deploying pet-web image version: $WEB_IMAGE_VERSION"
+CURRENT_VERSION=$(kubectl get deployment pet-web --namespace default -o jsonpath="{..image}" | cut -d':' -f2)
+echo "Current version: $CURRENT_VERSION"
+echo "Deploying pet-web canary image version: $IMAGE_VERSION"
 
-cat kubernetes/web.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$IMAGE_VERSION/" | kubectl apply -f - --namespace default
+cat kubernetes/web.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$CURRENT_VERSION/" | kubectl apply -f - --namespace default
+cat kubernetes/web-canary.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$IMAGE_VERSION/" | kubectl apply -f - --namespace default
