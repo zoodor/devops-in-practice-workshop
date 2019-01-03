0777cd5e84fae301ceac29a41a5a60c25864549e
Mark Taylor <mtaylor@thoughtworks.com>
2019-01-03 15:24:47 +0000

    Updated to deploy to Google Cloud as part of GoCD pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -xe
-kubectl apply -f kubernetes/mysql.yml
-kubectl apply -f kubernetes/web.yml
+
+echo "Deploying pet-db..."
+kubectl apply -f kubernetes/mysql.yml --namespace default
+
+IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
+PROJECT_ID=${GCLOUD_PROJECT_ID:-devops-workshop-112233}
+echo "Deploying pet-web image version: $IMAGE_VERSION"
+
+cat kubernetes/web.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$IMAGE_VERSION/" | kubectl apply -f - --namespace default
